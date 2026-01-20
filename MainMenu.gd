da9dd2b9e8add2a2a72fbca83fda2e905d5423f0
authenticBloodshed <robinvos2002@gmail.com>
2026-01-20 17:21:22 +0100

    Started working on fade animations
    options back to main menu screen is currently broken. Animation works but it doesn't transition back to the main screen sort some reason

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,17 +1,33 @@
 extends Node2D
 
+var buttonType = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$ColorRect.show()
+	$ColorRect/AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.0).timeout
+	$ColorRect.hide()
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	buttonType = "Start"
+	$ColorRect.show()
+	$ColorRect/fadeTimer.start()
+	$ColorRect/AnimationPlayer.play("fade_in")
 
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://optionsMenu.tscn")
-
+	buttonType = "Options"
+	$ColorRect.show()
+	$ColorRect/fadeTimer.start()
+	$ColorRect/AnimationPlayer.play("fade_in")
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_fade_timer_timeout():
+	if buttonType == "Start" :
+		get_tree().change_scene_to_file("res://game.tscn")
+		
+	elif buttonType == "Options" :
+		get_tree().change_scene_to_file("res://optionsMenu.tscn")
--- optionsMenu.gd
+++ optionsMenu.gd
@@ -1,15 +1,25 @@
 extends Node2D
 
+var buttonType = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$ColorRect.show()
+	$ColorRect/AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.0).timeout
+	$ColorRect.hide()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
+		buttonType = "Back"
+		$ColorRect.show()
+		$ColorRect/fadeTimer.start()
+		$ColorRect/AnimationPlayer.play("fade_in")
+		await get_tree().create_timer(1.0).timeout
+		$ColorRect.hide()
+
+func _on_fade_timer_timeout():
+	if buttonType == "Back" :
+		get_tree().change_scene_to_file("res://MainMenu.gd")
