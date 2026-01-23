39294f451a4c046029cec6cda42fa83531701f14
authenticBloodshed <robinvos2002@gmail.com>
2026-01-23 14:05:47 +0100

    Breakfast popup
    Puzzle now spawns upon opening the game and is able to be closed by pressing the close button

--- game.gd
+++ game.gd
@@ -4,3 +4,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ColorRect/AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(1.0).timeout
+	$ColorRect.hide()
+	$Breakfast.show()
+	$CloseButton.show()
+
+func _on_close_button_pressed():
+	$Breakfast.hide()
+	$CloseButton.hide()
