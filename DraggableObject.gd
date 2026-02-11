0ae5b6878ae408861df89a25f0b5922103988ac8
authenticBloodshed <robinvos2002@gmail.com>
2026-02-11 11:36:42 +0100

    Drag and drop systeem
    Added drag and drop system to the preperation station which works with placeholder assets. It does not yet check if all the ingredients are placed into the bowl or not nor does it give points yet. It is only the dragging and dropping itself that works

---
+++ DraggableObject.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+var isDragging := false
+var isPlaced := false
+var dragOffset := Vector2.ZERO
+
+func _ready():
+	input_pickable = true
+
+func _input_event(viewport, event, shape_idx):
+	
+	# Detects that the left mouse button is being clicked
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			
+			# Start dragging
+			if event.pressed and not isPlaced:
+				isDragging = true
+				dragOffset = global_position - get_global_mouse_position()
+			
+			# Releases and places ingredient
+			elif not event.pressed and isDragging:
+				isDragging = false
+				isPlaced = true   # Locks the ingredient in place and prevents it from being picked up again
+
+func _process(delta):
+	if isDragging:
+		global_position = get_global_mouse_position() + dragOffset
+
