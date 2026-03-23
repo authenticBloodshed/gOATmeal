aeffd2f595de3e36b0bdb8857ab749ad40874ba0
authenticBloodshed <robinvos2002@gmail.com>
2026-03-23 16:04:18 +0100

    Filters now work for dish types
    Filters should now theoretically work for dish types (but only for the dish types and not for anything else in the scene)

--- ToggleManager.gd
+++ ToggleManager.gd
@@ -1,30 +1,58 @@
 extends Node2D
 
+var activeFilters = {
+	"yogurt": false,
+	"oatmeal": false,
+	"other": false
+}
 
-# Called when the node enters the scene tree for the first time.
+var allDishes = [
+	{ "name": "Strawberry Yogurt", "type": "yogurt" },
+	{ "name": "Banana Oatmeal", "type": "oatmeal" },
+	{ "name": "Chocolate Yogurt", "type": "yogurt" },
+	{ "name": "Mystery Dish", "type": "other" }
+]
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-
 func _on_all_button_toggled(toggled_on):
-	$TabContainer/Type/YogurtButton.button_pressed = false
-	$TabContainer/Type/OatmealButton.button_pressed = false
-	$TabContainer/Type/OtherButton.button_pressed = false
+	if toggled_on:
+		activeFilters["yogurt"] = false
+		activeFilters["oatmeal"] = false
+		activeFilters["other"] = false
+		$TabContainer/Type/YogurtButton.button_pressed = false
+		$TabContainer/Type/OatmealButton.button_pressed = false
+		$TabContainer/Type/OtherButton.button_pressed = false
+	
+	apply_filters()
 
 func _on_yogurt_button_toggled(toggled_on):
-	$TabContainer/Type/AllButton.button_pressed = false
+	activeFilters["yogurt"] = toggled_on
+	
+	if toggled_on:
+		$TabContainer/Type/AllButton.button_pressed = false
+	
+	apply_filters()
 
 func _on_oatmeal_button_toggled(toggled_on):
-	$TabContainer/Type/AllButton.button_pressed = false
+	activeFilters["oatmeal"] = toggled_on
+	
+	if toggled_on:
+		$TabContainer/Type/AllButton.button_pressed = false
+	
+	apply_filters()
 
 func _on_other_button_toggled(toggled_on):
-	$TabContainer/Type/AllButton.button_pressed = false
+	activeFilters["other"] = toggled_on
+	
+	if toggled_on:
+		$TabContainer/Type/AllButton.button_pressed = false
+	
+	apply_filters()
 
 func _on_none_button_toggled(toggled_on):
 	$"TabContainer/Diet preferences/VegetarianButton".button_pressed = false
@@ -39,6 +67,7 @@ func _on_none_button_toggled(toggled_on):
 	$"TabContainer/Diet preferences/HalalButton".button_pressed = false
 	$"TabContainer/Diet preferences/KosherButton".button_pressed = false
 	$"TabContainer/Diet preferences/SeasonalButton".button_pressed = false
+
 
 func _on_vegetarian_button_toggled(toggled_on):
 	$"TabContainer/Diet preferences/NoneButton".button_pressed = false
@@ -75,3 +104,21 @@ func _on_kosher_button_toggled(toggled_on):
 
 func _on_seasonal_button_toggled(toggled_on):
 	$"TabContainer/Diet preferences/NoneButton".button_pressed = false
+
+func apply_filters():
+	var filteredDishes = []
+	var anyFilterActive = false
+	
+	for key in activeFilters:
+		if activeFilters[key]:
+			anyFilterActive = true
+			break
+	
+	for dish in allDishes:
+		if $TabContainer/Type/AllButton.button_pressed or not anyFilterActive:
+			filteredDishes.append(dish)
+		else:
+			if activeFilters[dish["type"]]:
+				filteredDishes.append(dish)
+	
+	print("Filtered dishes: ", filteredDishes)
