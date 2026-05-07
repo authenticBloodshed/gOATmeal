ff3170d3129dd09d25d8af18780e1d9985cb31c1
authenticBloodshed <robinvos2002@gmail.com>
2026-05-07 14:44:09 +0200

    Fixed layering issues
    Last grabbed ingredient is now always on top, regardless of it's place in the hierarchy

--- GranolaChunk.gd
+++ GranolaChunk.gd
@@ -1,8 +1,12 @@
 extends Node2D
-
+static var top_z := 0
 @export var texture: Texture2D
 @onready var sprite: Sprite2D = $Sprite2D
 var dragging := true
+
+func _ready():
+	top_z += 1
+	z_index = top_z
 
 func _process(_delta):
 	if dragging:
