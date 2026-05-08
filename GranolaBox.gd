8bdcb4d2f9b05fb620c16635288aa4d8f18c39fc
authenticBloodshed <robinvos2002@gmail.com>
2026-05-08 13:52:08 +0200

    Undo button added
    Added undo button to the game

--- GranolaBox.gd
+++ GranolaBox.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 @export var granolaChunkScene: PackedScene
-
+static var spawnedChunks = []
 @onready var spawnContainer = $SpawnedGranola
 @onready var boxArea = $Box
 
@@ -18,4 +18,14 @@ func _on_box_clicked(viewport, event, shape_idx):
 func spawn_granola():
 	var chunk = granolaChunkScene.instantiate()
 	spawnContainer.add_child(chunk)
+	spawnedChunks.append(chunk)
 	chunk.global_position = get_global_mouse_position()
+	
+func undo_last():
+	if spawnedChunks.size() > 0:
+		var lastChunk = spawnedChunks.pop_back()
+		lastChunk.queue_free()
+
+
+func _on_undo_button_pressed():
+	undo_last()
