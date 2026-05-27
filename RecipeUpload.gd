aa50b7101e58e5e42fb6a4b4d5bc9cc5c597ce6a
authenticBloodshed <robinvos2002@gmail.com>
2026-05-27 13:27:29 +0200

    Basic Upload sytem done
    Mockup of the upload system is done, but does not safe any images YET

--- RecipeUpload.gd
+++ RecipeUpload.gd
@@ -1,5 +1,5 @@
 extends Control
-
+var selectedImagePath = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +10,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_upload_image_button_pressed():
+	$ImageFileDialog.popup_centered()
+
+
+func _on_image_file_dialog_file_selected(path):
+	selectedImagePath = path
+
+	var image = Image.new()
+	image.load(path)
+
+	var texture = ImageTexture.create_from_image(image)
+
+	$ImageLabel/RecipePreview.texture = texture
+
+
+func _on_save_recipe_button_pressed():
+	print("Recipe saved!") #REPLACE LATER
